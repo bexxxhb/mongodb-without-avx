450a795459bf4684937c13c5c1ecb2bea5810643
m.klaas <super_drei@mailbox.org>
2025-02-07 19:10:10 +0100

    docker initdb file moved into custom directory

---
+++ docker-entrypoint-initdb.d/init-db.js
@@ -0,0 +1,37 @@
+dbprod = db.getSiblingDB("showcompare-prod");
+
+dbprod.createUser({
+    user: "scompProd",
+    pwd: "rqoKPi0Ept3$",
+    roles: [
+      {
+        role: 'readWrite', 
+        db: 'showcompare-prod'
+      },
+    ],
+  });
+
+dbprod.createCollection("movie");
+dbprod.createCollection("movieDetails");
+dbprod.createCollection("show");
+dbprod.createCollection("showEpisode");
+dbprod.createCollection("watchlistEntry");
+
+dbdev = db.getSiblingDB("showcompare-dev");
+
+dbdev.createUser({
+    user: "scompDev",
+    pwd: "scompDev",
+    roles: [
+      {
+        role: 'readWrite', 
+        db: 'showcompare-dev'
+      },
+    ],
+  });
+
+dbdev.createCollection("movie");
+dbdev.createCollection("movieDetails");
+dbdev.createCollection("show");
+dbdev.createCollection("showEpisode");
+dbdev.createCollection("watchlistEntry");
